e644833354463a0a125af5e82cd5f9f645f5b46f
Muhammad Tariq <tariqsohail1110@gmail.com>
2026-03-24 21:41:29 +0500

    feat: Add optional status field to GetPatientResponseDto for improved patient status tracking

--- apps/calendax-api/src/app/patient/dtos/get-patient-request.dto.ts
+++ apps/calendax-api/src/app/patient/dtos/get-patient-request.dto.ts
@@ -1,4 +1,5 @@
 import { SiteResponseDto } from "../../site/dtos/site-response.dto";
+import { PatientStatusEnum } from "../../utils/value-objects/patient-status.enum";
 import { UserStatus } from "../../utils/value-objects/user-status.vo";
 
 export class GetUserPatientResponseDto {
@@ -17,6 +18,7 @@ export class GetPatientResponseDto {
     id: number;
     address?: string;
     createdAt?: Date;
+    status?: PatientStatusEnum;
     user?: GetUserPatientResponseDto;
     isActive: boolean;
     sitesAssignments?: {
